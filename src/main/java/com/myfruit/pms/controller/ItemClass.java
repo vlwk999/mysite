a44803922e94455c94c7157c54bb3d694331fe31
viwk(nari) <tmsn1636@gmail.com>
2025-01-16 12:04:32 +0900

    아이템 가져오기

--- src/main/java/com/myfruit/pms/controller/ItemClass.java
+++ src/main/java/com/myfruit/pms/controller/ItemClass.java
@@ -1,15 +1,29 @@
 package com.myfruit.pms.controller;
 
+import com.myfruit.pms.dto.ItemDto;
+import com.myfruit.pms.mapper.ItemMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/items")
 public class ItemClass {
 
+    @Autowired
+    private ItemMapper itemMapper;
+
     @GetMapping("/create")
     public String create() {
         return "shop/create-item";
     }
+
+    @PostMapping
+    public void createItem(@RequestBody ItemDto itemDto){
+        //@RequestBody itemDto가
+         System.out.println(itemDto.getItem());
+         itemMapper.insertItem(itemDto);
+         //여기 itemDto 로 전달 그리고 itemMapper 인터페이스에 itemDto 로 전달
+    }
+
 }
