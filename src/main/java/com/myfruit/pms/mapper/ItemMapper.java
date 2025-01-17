918777fc6a266d1b7f376b9e8b063d859ee1f75a
viwk(nari) <tmsn1636@gmail.com>
2025-01-17 11:17:10 +0900

    게시판을 위한 맵퍼 구성

--- src/main/java/com/myfruit/pms/mapper/ItemMapper.java
+++ src/main/java/com/myfruit/pms/mapper/ItemMapper.java
@@ -3,9 +3,16 @@ package com.myfruit.pms.mapper;
 import com.myfruit.pms.dto.ItemDto;
 import org.apache.ibatis.annotations.Mapper;
 
-@Mapper
-public interface ItemMapper {
+import java.util.List;
 
-    void insertItem(ItemDto itemDto);
+@Mapper
+public interface ItemMapper { //인터페이스의 기본은 public
+
+    void insertItem(ItemDto itemDto); //Pk 생성
     // itemClass itemMapper.insertItem(itemDto) 전달받고
+    ItemDto getItemById(int id); //Pk가 생성, int 성공 또는 실패
+    // 1개이상 나오는거는 list
+    List<ItemDto> getItems();
+    void updateItem(ItemDto itemDto); // 내용 변경
+    void deleteItem(int id);
 }
