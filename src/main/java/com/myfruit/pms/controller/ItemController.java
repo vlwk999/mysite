f667518c241936012956342de866cc89402d4799
viwk(nari) <tmsn1636@gmail.com>
2025-01-17 11:45:06 +0900

    요청주소 설계 - 컨트롤러

--- src/main/java/com/myfruit/pms/controller/ItemController.java
+++ src/main/java/com/myfruit/pms/controller/ItemController.java
@@ -4,7 +4,9 @@ import com.myfruit.pms.dto.ItemDto;
 import com.myfruit.pms.mapper.ItemMapper;
 import com.myfruit.pms.service.ItemService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
@@ -30,9 +32,24 @@ public class ItemController {
     }
 
     @GetMapping("/{id}")
-    @ResponseBody
-    public ItemDto getItem(@PathVariable("id") int id){
-        return itemService.getItem(id);
+    public String getItem(@PathVariable("id") int id, Model model){
+        ItemDto itemDto = itemService.getItem(id);
+        model.addAttribute("item", itemDto);
+
+        return "shop/detail";
     }
 
+    // 생성 페이지 GET / items /create
+    // 생성 POST / items
+    // 상세보기 GET / items /{id}
+    // 수정 POST / items /{id}
+    // 삭제 GET / items/{id}/delete
+
+    // 생성 페이지 GET / items /create
+    // 생성 POST / items
+    // 상세보기 GET / items /{id}
+    // 수정 PUT / items /{id}
+    // 삭제 DELETE /items/{id}
+
 }
+
