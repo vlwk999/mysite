72a1055332ae11cfbc8fe8d9af318ae07cdebbc6
viwk(nari) <tmsn1636@gmail.com>
2025-01-17 12:27:48 +0900

    상세보기 예외처리 및 목록보기 구현

--- src/main/java/com/myfruit/pms/controller/ItemController.java
+++ src/main/java/com/myfruit/pms/controller/ItemController.java
@@ -9,6 +9,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @Controller
 @RequestMapping("/items")
 public class ItemController {
@@ -33,23 +35,45 @@ public class ItemController {
 
     @GetMapping("/{id}")
     public String getItem(@PathVariable("id") int id, Model model){
-        ItemDto itemDto = itemService.getItem(id);
-        model.addAttribute("item", itemDto);
-
-        return "shop/detail";
+        // Model 객체로, 컨트롤러에서 뷰로 데이터를 전달
+        try {
+            ItemDto itemDto = itemService.getItem(id); // id를 사용해 아이템 정보를 가져옴
+            model.addAttribute("item", itemDto); // 아이템 정보를 모델에 추가
+        }catch (IllegalStateException e){
+            model.addAttribute("message",e.getMessage());
+            return "common/error/404";
+        }
+        // model.addAttribute 모델에 데이터를 추가하는 메소드 ->  데이터를 뷰로 전달
+        return "shop/detail"; // shop / detail 뷰를 반환
     }
+
+    @GetMapping
+    public String getItems(Model model) {
+        itemService.getItems();
+        List<ItemDto> items = itemService.getItems();
+        model.addAttribute("items",items);
+
+        return "shop/list";
+    }
+
+
+
+
+
+
 
     // 생성 페이지 GET / items /create
     // 생성 POST / items
     // 상세보기 GET / items /{id}
     // 수정 POST / items /{id}
     // 삭제 GET / items/{id}/delete
+    // 목록 Get /items
 
     // 생성 페이지 GET / items /create
     // 생성 POST / items
     // 상세보기 GET / items /{id}
     // 수정 PUT / items /{id}
     // 삭제 DELETE /items/{id}
-
+    // 목록 Get / items
 }
 
